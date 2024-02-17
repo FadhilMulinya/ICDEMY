78de624f50291c8032ea42fe39f796e7bf24dc92
TABBY-ARUH <cheminingnelly@gmail.com>
2024-02-17 19:19:52 +0300

    Add frontend files

---
+++ icdemy_documentation_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { icdemy_documentation_backend } from "../../declarations/icdemy_documentation_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await icdemy_documentation_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
